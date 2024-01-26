f47883a479782beafd846613297fe531d10a91d4
EliSauder <24995216+EliSauder@users.noreply.github.com>
2024-01-25 18:50:35 -0800

    refactor: Removed old configurations

--- after/plugin/neogen.lua
+++
@@ -1,6 +0,0 @@
-require('neogen').setup {
-    snippet_engine = "luasnip"
-}
-
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>dc", function() require('neogen').generate() end, opts)
